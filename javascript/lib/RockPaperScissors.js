810800777e8493db56ed1103bbedad8ae492a351
Robin <robindoble@gmail.com>
2014-05-13 11:02:40 +0100

    refactored winner(), loser() and result() methods to not require the use of global variables

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -41,25 +41,49 @@ Game.prototype.winner = function() {
 		return null;
 	}
 	else if	(PAIRS[this.player1.pick][this.player2.pick])  {	
-		winner = this.player1;
-		loser = this.player2;
+		// winner = this.player1;
+		// loser = this.player2;
 		return this.player1 }
 	else {
-		winner = this.player2;
-		loser = this.player1;
+		// winner = this.player2;
+		// loser = this.player1;
 	 	return this.player2;
 	 } 
 };
+
+Game.prototype.loser = function() {
+  return (this.winner() === this.player1 ? this.player2 : this.player1);
+}
 
 Game.prototype.same_pick = function() {
 	return this.player1.pick === this.player2.pick;
 };
 
 Game.prototype.result = function() {
-	console.log(this.player1)
-	console.log(winner.name+" playing "+winner.pick+" "+PAIRS[winner.pick][loser.pick]+" "+loser.name+" playing "+loser.pick);
-	return winner.name+" playing "+winner.pick+" "+PAIRS[winner.pick][loser.pick]+" "+loser.name+" playing "+loser.pick;
+	// console.log(this.player1)
+	var message;
+
+	if (this.winner()) {
+		message = [this.winner().name,"playing",this.winner().pick,PAIRS[this.winner().pick][this.loser().pick],this.loser().name,"playing",this.loser().pick].join(' ');
+	} else {
+		message = 'Draw';
+	}
+	console.log(message);
+	return message;	
+
+	// console.log(winner.name+" playing "+winner.pick+" "+PAIRS[winner.pick][loser.pick]+" "+loser.name+" playing "+loser.pick);
+	// return winner.name+" playing "+winner.pick+" "+PAIRS[winner.pick][loser.pick]+" "+loser.name+" playing "+loser.pick;
 
 };
+
+
+
+
+
+
+
+
+
+
 
 
